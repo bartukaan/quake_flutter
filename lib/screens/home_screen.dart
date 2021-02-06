f1a54f5f6197c26fa10e879023f6491599f9f904
bartukaan <Bartu1907+>
2021-02-06 13:41:55 +0300

    [UPD]  -- liste elemanları düzenleme

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,6 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:quake_flutter/screens/quake_bag.dart';
 import 'package:quake_flutter/utilities/constants.dart';
-
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -19,24 +16,19 @@ class HomeScreen extends StatelessWidget {
           flexibleSpace: FlexibleSpaceBar(
             title: Text("Deprem App"),
             centerTitle: true,
-          /*  background: Image.asset(
+            /*  background: Image.asset(
               "assets/images/image.jpg",
               fit: BoxFit.cover,
             ),*/
           ),
         ),
 
-
-
         //sabit elemanlarla bir satırda kaç eleman olacağını söylediğimiz grid türü
         SliverGrid(
           gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
           delegate: SliverChildListDelegate(sabitListeElemanlari(context)),
         ),
-
-
-
       ],
     );
   }
@@ -44,63 +36,20 @@ class HomeScreen extends StatelessWidget {
   //listeler için tanımlanmış elemanları döndüren fonksiyon
   List<Widget> sabitListeElemanlari(BuildContext context) {
     return [
-     _buildBagRequired(context),
+      _buildBagRequired(context),
       _buildLastQuakes(context),
-      Container(
-        height: 100,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.blue),
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        child: Text(
-          "Sabit Liste Elemanı 3",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-      ),
-      Container(
-        height: 100,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.orange),
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        child: Text(
-          "Sabit Liste Elemanı 4",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-      ),
-      Container(
-        height: 100,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.purple),
-        alignment: Alignment.center,
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        child: Text(
-          "Sabit Liste Elemanı 5",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-      ),
-      Container(
-        height: 100,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.cyan),
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        child: Text(
-          "Sabit Liste Elemanı 6",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-      ),
+      _buildThirdContainer(context),
+      _buildForthContainer(context),
+      _buildFifthContainer(context),
+      _buildSixthContainer(context),
     ];
   }
 
-  _buildLastQuakes(BuildContext context){
-    return       Container(
+  _buildLastQuakes(BuildContext context) {
+    return Container(
       height: 100,
-      decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.teal),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.teal),
       alignment: Alignment.center,
       margin: EdgeInsets.all(10),
       padding: EdgeInsets.all(10),
@@ -112,27 +61,92 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-_buildBagRequired(BuildContext context){
-    return  /*InkWell(
+  _buildBagRequired(BuildContext context) {
+    return /*InkWell(
       onTap: () {
         Navigator.push(context, MaterialPageRoute(
             builder: (context) => QuakeBag()
         ));
       },
 
-      child:*/ Container(
-        margin: EdgeInsets.all(10),
-        padding: EdgeInsets.all(10),
-        height: 100,
-        alignment: Alignment.center,
-        decoration: BoxDecoration(borderRadius: BorderRadius.circular(30),color: Colors.amber),
-        child: Text(
-          "Deprem Çantası",
-          style: TextStyle(fontSize: 18),
-          textAlign: TextAlign.center,
-        ),
-      );
- //   );
-}
+      child:*/
+        Container(
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      height: 100,
+      alignment: Alignment.center,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.amber),
+      child: Text(
+        "Deprem Çantası",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+    //   );
+  }
 
+  _buildThirdContainer(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.blue),
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        "Sabit Liste Elemanı 3",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  _buildForthContainer(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.orange),
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        "Sabit Liste Elemanı 4",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  _buildFifthContainer(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.cyan),
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.center,
+      child: Text(
+        "Sabit Liste Elemanı 6",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  _buildSixthContainer(BuildContext context) {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30), color: Colors.purple),
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(10),
+      padding: EdgeInsets.all(10),
+      child: Text(
+        "Sabit Liste Elemanı 5",
+        style: TextStyle(fontSize: 18),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
 }
